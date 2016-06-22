de5adb6992b5d7945e0ba0bf66f8e51416b860c5
Jeremy Raymond <jeremy.r.raymond@gmail.com>
2016-06-22 14:30:22 -0400

    Added custom end point to return post objects when a url is given

---
+++ wp-api-posts-from-url.php
@@ -0,0 +1,27 @@
+
+<?php
+/**
+ * Plugin Name: WP API Posts from URL
+ * Plugin URI: https://github.com/jeremyraymond/wp-api-posts-from-url
+ * Description: Extends wordpress api to returns post/page information based on a url rather than id or slug.
+ * Version: 1.0.0
+ * Author: Jeremy Raymond
+ * Author URI: http://jeremy-raymond.com
+ * License: GPL2
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+function get_posts_from_url( $request ) {
+    $params = $request->get_query_params();
+    $postid = url_to_postid( $params['url'] );
+    $post = get_post($postid);
+    return $post;
+}
+
+add_action( 'rest_api_init', function () {
+    register_rest_route( 'posts-from-url/v1', '/test', array(
+        'methods' => 'GET',
+        'callback' => 'get_posts_from_url',
+    ) );
+} );
